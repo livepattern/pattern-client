daf90b263612062a57ba8c2e1b5deabd47f7639e
Jeff Stock <jeffcstock@gmail.com>
2018-09-22 14:03:04 -0700

    test the case sensitive issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import "./App.css"
-import Home from "./views/Home"
+import Home from "./views/home"
 import TC from './views/TC'
 import Order from "./views/Order"
 import Header from './views/components/Header'
